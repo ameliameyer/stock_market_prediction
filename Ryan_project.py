415ee37821f73655a9bf3d4af737d43c8259b52a
rspyro <ryanschwendinger@yahoo.com>
2022-11-05 17:48:10 -0700

    adding my version of stock predictor.
    Still need to work on the predicted section meshing with actual values.

---
+++ Ryan_project.py
@@ -0,0 +1,76 @@
+from pandas_datareader import data as pdr
+from datetime import datetime
+import datetime as dt
+import time
+import math
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+import yfinance as yf
+
+style.use('ggplot')
+yf.pdr_override()
+
+ticker = input("What Stock ticker would you like to predict:")
+ticker = [ticker]
+
+end = datetime.now(tz=None)
+start = datetime(end.year - 1, end.month, end.day)
+
+df = pdr.get_data_yahoo(ticker, start, end)
+df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
+df['HL_PCT'] = (df['High'] - df['Close']) / df['Close'] * 100
+df['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100
+df = df[['Close', 'HL_PCT', 'PCT_change', 'Volume']]
+
+forecast_col = 'Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.03*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,train_size=0.3, random_state=25)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+confidence = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+
+df['forecast'] = np.nan
+
+"""print(forecast_out, forecast_set)"""
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = dt.datetime.fromtimestamp(next_unix, dt.timezone.utc)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Close'].plot()
+df['forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
